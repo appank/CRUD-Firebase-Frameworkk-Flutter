fb9764c16c716db07a76d28fb5478a43f699eb26
basoarfanefendy172263@gmail.com <basoarfanefendy172263@gmail.com>
2023-12-27 17:39:31 +0800

    fixx

--- lib/Class/databasehalper_reltime.dart
+++ lib/Class/databasehalper_reltime.dart
@@ -1,60 +1,20 @@
-import 'package:firebase_database/firebase_database.dart';
+class Student {
+  String? key;
+  StudentData? studentData;
 
-final DatabaseReference databaseReference =
-    FirebaseDatabase.instance.reference();
-
-Future<void> addData(String name, String email) async {
-  String? key = databaseReference.child('users').push().key;
-  await databaseReference.child('users').child(key!).set({
-    'name': name,
-    'email': email,
-  });
+  Student({this.key, this.studentData});
 }
 
-Stream<List<User>> getUsers() {
-  return databaseReference.child('users').onValue.map((event) {
-    List<User> userList = [];
+class StudentData {
+  String? name;
+  String? age;
+  String? subject;
 
-    // Pengecekan tipe data snapshot.value
-    if (event.snapshot.value is Map<String, dynamic>) {
-      // Konversi snapshot.value ke Map<String, dynamic>
-      Map<String, dynamic> data = event.snapshot.value as Map<String, dynamic>;
+  StudentData({this.name, this.age, this.subject});
 
-      // Iterasi melalui entri Map
-      data.forEach((key, value) {
-        userList.add(User.fromJson(key, value));
-      });
-    }
-
-    return userList;
-  });
-}
-
-// Update Data
-Future<void> updateData(String key, String name, String email) async {
-  await databaseReference.child('users').child(key).update({
-    'name': name,
-    'email': email,
-  });
-}
-
-// Delete Data
-Future<void> deleteData(String key) async {
-  databaseReference.child('users').child(key).remove();
-}
-
-class User {
-  String key;
-  String name;
-  String email;
-
-  User(this.key, this.name, this.email);
-
-  factory User.fromJson(String key, Map<String, dynamic> json) {
-    return User(
-      key,
-      json['name'] as String,
-      json['email'] as String,
-    );
+  StudentData.fromJson(Map<dynamic, dynamic> json) {
+    name = json["name"];
+    age = json["age"];
+    subject = json["subject"];
   }
 }
--- lib/screens/input_data_Reltime.dart
+++ lib/screens/input_data_Reltime.dart
@@ -1,52 +1,181 @@
 import 'package:crud_firebase/Class/databasehalper_reltime.dart';
-import 'package:crud_firebase/screens/read_data.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
-class AddDataScreen extends StatefulWidget {
+class InputDataReltime extends StatefulWidget {
+  const InputDataReltime({Key? key}) : super(key: key);
+
   @override
-  _AddDataScreenState createState() => _AddDataScreenState();
+  State<InputDataReltime> createState() => _InputDataReltimeState();
 }
 
-class _AddDataScreenState extends State<AddDataScreen> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController emailController = TextEditingController();
+class _InputDataReltimeState extends State<InputDataReltime> {
+  DatabaseReference dbRef = FirebaseDatabase.instance.ref();
+
+  final TextEditingController _edtNameController = TextEditingController();
+  final TextEditingController _edtAgeController = TextEditingController();
+  final TextEditingController _edtSubjectController = TextEditingController();
+
+  List<Student> studentList = [];
+
+  bool updateStudent = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    retrieveStudentData();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tambah Data'),
+        title: const Text("Student Directory"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            TextField(
-              controller: nameController,
-              decoration: InputDecoration(labelText: 'Nama'),
+            for (int i = 0; i < studentList.length; i++)
+              studentWidget(studentList[i])
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _edtNameController.text = "";
+          _edtAgeController.text = "";
+          _edtSubjectController.text = "";
+          updateStudent = false;
+          studentDialog();
+        },
+        child: const Icon(Icons.save),
+      ),
+    );
+  }
+
+  void studentDialog({String? key}) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return Dialog(
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: _edtNameController,
+                    decoration: const InputDecoration(helperText: "Name"),
+                  ),
+                  TextField(
+                      controller: _edtAgeController,
+                      decoration: const InputDecoration(helperText: "Age")),
+                  TextField(
+                      controller: _edtSubjectController,
+                      decoration: const InputDecoration(helperText: "Subject")),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        Map<String, dynamic> data = {
+                          "name": _edtNameController.text.toString(),
+                          "age": _edtAgeController.text.toString(),
+                          "subject": _edtSubjectController.text.toString()
+                        };
+
+                        if (updateStudent) {
+                          dbRef
+                              .child("Students")
+                              .child(key!)
+                              .update(data)
+                              .then((value) {
+                            int index = studentList
+                                .indexWhere((element) => element.key == key);
+                            studentList.removeAt(index);
+                            studentList.insert(
+                                index,
+                                Student(
+                                    key: key,
+                                    studentData: StudentData.fromJson(data)));
+                            setState(() {});
+                            Navigator.of(context).pop();
+                          });
+                        } else {
+                          dbRef
+                              .child("Students")
+                              .push()
+                              .set(data)
+                              .then((value) {
+                            Navigator.of(context).pop();
+                          });
+                        }
+                      },
+                      child: Text(updateStudent ? "Update Data" : "Save Data"))
+                ],
+              ),
             ),
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(labelText: 'Email'),
+          );
+        });
+  }
+
+  void retrieveStudentData() {
+    dbRef.child("Students").onChildAdded.listen((data) {
+      StudentData studentData =
+          StudentData.fromJson(data.snapshot.value as Map);
+      Student student =
+          Student(key: data.snapshot.key, studentData: studentData);
+      studentList.add(student);
+      setState(() {});
+    });
+  }
+
+  Widget studentWidget(Student student) {
+    return InkWell(
+      onTap: () {
+        _edtNameController.text = student.studentData!.name!;
+        _edtAgeController.text = student.studentData!.age!;
+        _edtSubjectController.text = student.studentData!.subject!;
+        updateStudent = true;
+        studentDialog(key: student.key);
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(5),
+        margin: const EdgeInsets.only(top: 5, left: 10, right: 10),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(color: Colors.black)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(student.studentData!.name!),
+                Text(student.studentData!.age!),
+                Text(student.studentData!.subject!),
+              ],
             ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // Panggil fungsi untuk menambahkan data ke Firebase
-                addData(nameController.text, emailController.text);
-              },
-              child: Text('Tambahkan Data'),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ViewDataScreen()),
-                );
-              },
-              child: Text('Tambahkan Data'),
-            ),
+            InkWell(
+                onTap: () {
+                  dbRef
+                      .child("Students")
+                      .child(student.key!)
+                      .remove()
+                      .then((value) {
+                    int index = studentList
+                        .indexWhere((element) => element.key == student.key!);
+                    studentList.removeAt(index);
+                    setState(() {});
+                  });
+                },
+                child: const Icon(
+                  Icons.delete,
+                  color: Colors.red,
+                ))
           ],
         ),
       ),
